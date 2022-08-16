34d4b6ff2a2c14c2d99ccf618b0d18439b835c61
abreumatheu <matheus-abreu10@hotmail.com>
2022-08-15 22:36:52 -0300

    versão1

---
+++ mesa07.js
@@ -0,0 +1,28 @@
+let num1 = 15;
+let num2 = 10;
+let num3 = 4;
+let num4 = 1;
+
+console.log ( num1 - num2 - num3 - num4 );
+
+// console.log (num1 > num2);
+// console.log (num1 < num2);
+// console.log (num2 < num1 < num3 );
+// console.log (num2 < num1 < num3 );
+// console.log ( num1 - num2 - num3 );
+// console.log ( num1 - num2 - num3 );
+// console.log ( num1 - num2 - num3 - num4 );
+// console.log ( num1 - num2 - num3 - num4 );
+
+// let x = 5;
+// let y = 9;
+
+// a. console.log (x < 10 && x!==5);
+// b. console.log ( x>9 || x===5);
+// c. console.log ( !(x===y));
+
+// ex2
+
+// 1. False
+// 2. False
+// 3. False
